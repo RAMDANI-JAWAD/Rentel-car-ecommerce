5d079f9de4d4d6a0ca40debb9e48bc1846f03dfe
jawad ramdani <jawad.ramdani99@gmail.com>
2026-06-19 15:04:50 +0000

    update CORS configuration to use CLIENT_ORIGIN from .env.example

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_ORIGIN }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'client', 'public')));
 
